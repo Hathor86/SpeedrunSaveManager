87f416109c5cba24a66051aa096ccf2c0143cc4c
Hathor86 <francois.guiot@gmail.com>
2016-05-29 20:55:10 +0200

    Initial upload

---
+++ CustomMainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace BizHawk.Client.EmuHawk
+{
+	partial class CustomMainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomMainForm));
+			this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.treeView = new System.Windows.Forms.TreeView();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.tableLayoutPanel.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel
+			// 
+			this.tableLayoutPanel.ColumnCount = 2;
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel.Controls.Add(this.treeView, 0, 0);
+			this.tableLayoutPanel.Controls.Add(this.pictureBox1, 1, 0);
+			this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel.Name = "tableLayoutPanel";
+			this.tableLayoutPanel.RowCount = 2;
+			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel.Size = new System.Drawing.Size(457, 361);
+			this.tableLayoutPanel.TabIndex = 0;
+			// 
+			// treeView
+			// 
+			this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeView.Location = new System.Drawing.Point(3, 3);
+			this.treeView.Name = "treeView";
+			this.tableLayoutPanel.SetRowSpan(this.treeView, 2);
+			this.treeView.Size = new System.Drawing.Size(222, 355);
+			this.treeView.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(231, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(223, 174);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// CustomMainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(457, 361);
+			this.Controls.Add(this.tableLayoutPanel);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "CustomMainForm";
+			this.Text = "Speedrun Save Manager";
+			this.tableLayoutPanel.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+		private System.Windows.Forms.TreeView treeView;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
---
+++ CustomMainForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BizHawk.Client.ApiHawk;
+using BizHawk.Client.ApiHawk.Classes.Events;
+using BizHawk.Client.Common;
+
+namespace BizHawk.Client.EmuHawk
+{
+	public partial class CustomMainForm : Form, IExternalToolForm
+	{
+		#region Fields
+		#endregion
+
+		public CustomMainForm()
+		{
+			InitializeComponent();
+			romload(null, null);			
+
+			treeView.NodeMouseClick += new TreeNodeMouseClickEventHandler(TreeNode_Click);
+			treeView.NodeMouseHover += new TreeNodeMouseHoverEventHandler(TreeNode_MouseHover);			
+
+			ClientApi.RomLoaded += new EventHandler(romload);
+			ClientApi.BeforeQuickSave += new BeforeQuickSaveEventHandler(BeforeQuickSave);
+		}		
+
+		public void romload(object sender, EventArgs e)
+		{
+			string basePath;
+
+			treeView.Nodes.Clear();
+
+			TreeNode main = new TreeNode(Global.Game.Name);
+			treeView.Nodes.Add(main);
+			/*basePath = Global.Game.Name;
+			foreach(char c in Path.GetInvalidPathChars())
+			{
+				basePath = basePath.Replace(c, '_');
+			}*/
+			basePath = Path.Combine(PathManager.GetSaveStatePath(Global.Game), Global.Game.Name);
+
+			if (!Directory.Exists(basePath))
+			{
+				Directory.CreateDirectory(basePath);
+			}
+			else
+			{
+				PopulateTreeView(basePath, main);
+			}
+			treeView.ExpandAll();
+		}
+
+		private void PopulatePictureBox(BinaryReader reader)
+		{
+			pictureBox1.Image = new Bitmap(reader.BaseStream);
+		}
+
+		private void PopulateTreeView(string path, TreeNode node)
+		{
+			DirectoryInfo current = new DirectoryInfo(path);
+			foreach(DirectoryInfo di in current.GetDirectories())
+			{
+				TreeNode child = new TreeNode(di.Name);
+				node.Nodes.Add(child);
+				if (di.GetDirectories().Any())
+				{
+					PopulateTreeView(di.FullName, child);
+				}
+				else
+				{
+					foreach(FileInfo savestate in di.GetFiles("*.State"))
+					{
+						child.Nodes.Add(new TreeNode(savestate.Name.Replace(".State", string.Empty)));
+					}
+				}
+			}
+		}
+
+		private void BeforeQuickSave(object sender, BeforeQuickSaveEventArgs e)
+		{
+			string path;
+			if(treeView.SelectedNode.Nodes.Count == 0)
+			{
+				path = Path.Combine(PathManager.GetSaveStatePath(Global.Game), treeView.SelectedNode.FullPath);
+			}
+			else
+			{
+				treeView.SelectedNode = treeView.SelectedNode.Parent;
+				path = Path.Combine(PathManager.GetSaveStatePath(Global.Game), treeView.SelectedNode.Parent.FullPath);
+			}
+			ClientApi.SaveState(Path.Combine(path, e.Name));
+			e.Handled = true;
+
+			//treeView.SelectedNode.Nodes.Add(new TreeNode(e.Name));
+		}
+
+		private void TreeNode_Click(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			if (treeView.SelectedNode.Nodes.Count == 0)
+			{
+				ClientApi.LoadState(Path.Combine(PathManager.GetSaveStatePath(Global.Game), e.Node.FullPath));
+			}
+		}
+
+		private void TreeNode_MouseHover(object sender, TreeNodeMouseHoverEventArgs e)
+		{
+			if (treeView.SelectedNode.Nodes.Count == 0)
+			{
+				BinaryStateLoader.LoadAndDetect(Path.Combine(PathManager.GetSaveStatePath(Global.Game), string.Format("{0}.State", e.Node.FullPath))).GetLump(BinaryStateLump.Framebuffer, false, PopulatePictureBox);
+			}
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			base.OnClosed(e);
+
+			ClientApi.BeforeQuickSave -= BeforeQuickSave;
+		}
+
+		#region Bizhawk required stuff
+		public bool UpdateBefore
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public bool AskSaveChanges()
+		{
+			return true;
+		}
+
+		public void FastUpdate()
+		{}
+
+		public void Restart()
+		{}
+
+		public void UpdateValues()
+		{}
+		#endregion
+	}
+}
